025d542c151f3c5c13a9fe3215fb3b5db1181e16
youngmin-gwon <youngmin-gwon@users.noreply.github.com>
2023-01-21 14:50:04 +0900

    chore(keyboard): change file name for clarity

--- keyboard/air40.c
+++ keyboard/air40.c
@@ -29,12 +29,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,       KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,            KC_U,    KC_I,    KC_O,    KC_P,             KC_BSPC,
         KC_TAB,       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,            KC_H,            KC_J,    KC_K,    KC_L,    KC_SCLN,          KC_ENT,
         KC_LSFT,      KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_N,            KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
-        LCTL(KC_SPC), KC_LCTL, KC_LALT, KC_LGUI, MO(1),   KC_SPC,  KC_ENT,  MO(2),   KC_RCTL, TG(_NUMPAD), KC_DOWN,          KC_RGHT
+        LCTL(KC_SPC), KC_LCTL, KC_LALT, KC_LGUI, MO(1),   KC_SPC,  KC_ENT,  MO(2),   KC_RCTL, TG(_NUMPAD),    KC_DOWN,          QK_LOCK
     ),
     [_LOWER] = LAYOUT_ortho_4x12(
         KC_TILD, KC_1,     KC_2,      KC_3,     KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
         _______, KC_COLN,  KC_MINS,   KC_EQL,   KC_DQUO, KC_QUOT, KC_GRV,  KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, KC_BSLS,
-        _______, KC_PERC,  KC_UNDS,   KC_PLUS,  KC_PIPE, KC_QUES, KC_PERC, KC_LBRC, KC_RBRC, KC_LT,   KC_GT,   _______,
+        _______, KC_SCLN,  KC_UNDS,   KC_PLUS,  KC_PIPE, KC_QUES, KC_PERC, KC_LBRC, KC_RBRC, KC_LT,   KC_GT,   _______,
         _______, _______,  _______,   _______,  _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
     ),
     [_RAISE] = LAYOUT_ortho_4x12(
@@ -49,10 +49,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______,  KC_MNXT, TG(_NUMPAD),    KC_VOLU, KC_MPLY
     ),
     [_ADJUST] = LAYOUT_ortho_4x12(
-        QK_BOOT, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______, _______, _______, _______, _______, DB_TOGG,
-        _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, AG_NORM, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+        _______, _______, RGB_HUI, RGB_SAI, RGB_RMOD, RGB_TOG, _______, KC_WH_D, _______, _______, _______, DB_TOGG,
+        KC_BTN2, _______, RGB_SPI, KC_WH_U, RGB_MOD,  _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, KC_BTN1,
+        _______, _______, _______, RGB_HUI, _______,  RGB_VAI, _______, _______, _______, _______, _______, _______,
+        QK_BOOT, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______
     )
 };
 

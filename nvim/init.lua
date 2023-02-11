f9e250aa678f61794f4be5efa99ec0cf92d73194
youngmin-gwon <youngmin-gwon@users.noreply.github.com>
2023-02-11 12:35:10 +0900

    config(nvim): add system clipboard feature per os

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,3 +5,18 @@ else
   -- ordinary Neovim
   require("user")
 end
+
+local has = vim.fn.has
+local is_mac = has "macunix"
+local is_win = has "win32"
+local is_wsl = has "wsl"
+
+if is_mac then
+  require('os.macos')
+end
+if is_win then
+  require('os.windows')
+end
+if is_wsl then
+  require('os.wsl')
+end

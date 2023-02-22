7d25132951895456d1b7ebb9ea03aad1468e0a2f
youngmin-gwon <youngmin-gwon@users.noreply.github.com>
2023-02-22 11:06:56 +0900

    config(keyboard): edit keymap

--- keyboard/air40.c
+++ keyboard/air40.c
@@ -32,13 +32,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LCTL(KC_SPC), LCTL_T(KC_MPLY),  KC_LALT, KC_LGUI, MO(1),   KC_SPC,  KC_ENT,  MO(2),   KC_LCTL, TG(_NUMPAD), _______, QK_LOCK
     ),
     [_LOWER] = LAYOUT_ortho_4x12(
-        KC_TILD, KC_1,     KC_2,      KC_3,     KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_GRV, KC_1,     KC_2,      KC_3,     KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
         _______, KC_COLN,  KC_MINS,   KC_EQL,   KC_DQUO, KC_QUOT, KC_GRV,  KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, KC_BSLS,
         _______, KC_SCLN,  KC_UNDS,   KC_PLUS,  KC_PIPE, KC_QUES, KC_PERC, KC_LBRC, KC_RBRC, KC_LT,   KC_GT,   _______,
         _______, _______,  _______,   _______,  _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, KC_SLEP 
     ),
     [_RAISE] = LAYOUT_ortho_4x12(
-        KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+        KC_TILD,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
         KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_RCBR, KC_PIPE,
         _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_LBRC, KC_RBRC, KC_PGUP, KC_PGDN, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BRID, KC_BRIU, KC_WAKE 

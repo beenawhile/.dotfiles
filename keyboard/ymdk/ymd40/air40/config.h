b2b37e5af1effbc9461cec3e10d4bc0edffe0c4a
youngmin-gwon <youngmin-gwon@users.noreply.github.com>
2023-05-29 19:00:42 +0900

    chore(keyboard): edit layout for air40

--- keyboard/ymdk/ymd40/air40/config.h
+++ keyboard/ymdk/ymd40/air40/config.h
@@ -90,7 +90,7 @@
 
 /// -- Home Row --
 // Configure the global tapping term (default: 200ms)
-#define TAPPING_TERM 200
+#define TAPPING_TERM 140
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
--- keyboard/ymdk/ymd40/air40/keymaps/youngmin-gwon/keymap.c
+++ keyboard/ymdk/ymd40/air40/keymaps/youngmin-gwon/keymap.c
@@ -19,44 +19,62 @@
 enum layer_names {
   _QWERTY,
   _LOWER,
-  _RAISE,
+  _NAV,
+  _ADJUST,
   _NUMPAD,
-  _ADJUST
+  _FUNC,
+  _MEDIA,
+  _MOUSE,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_ortho_4x12(
-        KC_ESC,       KC_Q,             KC_W,         KC_E,           KC_R,        KC_T,        KC_Y,        KC_U,        KC_I,           KC_O,           KC_P,    KC_BSPC,
-        KC_TAB,       KC_A,             ALT_T(KC_S),  GUI_T(KC_D),    SFT_T(KC_F), CTL_T(KC_G), CTL_T(KC_H), SFT_T(KC_J), GUI_T(KC_K),    ALT_T(KC_L),    KC_SCLN, KC_ENT,
-        KC_LSFT,      KC_Z,             ALGR_T(KC_X), KC_C,           KC_V,        KC_B,        KC_N,        KC_M,        KC_COMM,        ALGR_T(KC_DOT), KC_SLSH, KC_LSFT,
-        LCTL(KC_SPC), KC_LCTL,  KC_LALT,      KC_LGUI,        MO(1),       KC_SPC,      KC_ENT,      MO(2),       KC_LCTL,        TG(_NUMPAD),    _______, QK_LOCK
+        _______, KC_Q,         KC_W,         KC_E,              KC_R,              KC_T,               KC_Y,        KC_U,             KC_I,             KC_O,           KC_P,    _______,
+        _______, KC_A,         ALT_T(KC_S),  GUI_T(KC_D),       SFT_T(KC_F),       CTL_T(KC_G),        CTL_T(KC_H), SFT_T(KC_J),      GUI_T(KC_K),      ALT_T(KC_L),    KC_SCLN, KC_ENT,
+        _______, KC_Z,         ALGR_T(KC_X), KC_C,              KC_V,              KC_B,               KC_N,        KC_M,             KC_COMM,          ALGR_T(KC_DOT), KC_SLSH, _______,
+        _______, LCTL(KC_SPC), _______,      LT(_MEDIA,KC_ESC), LT(_LOWER,KC_TAB), LT(_MOUSE,KC_SPC),  KC_ENT,      LT(_NAV,KC_BSPC), LT(_FUNC,KC_DEL), _______,        _______, _______ 
     ),
     [_LOWER] = LAYOUT_ortho_4x12(
-        KC_GRV, KC_1,     KC_2,      KC_3,     KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        _______, KC_COLN,  KC_MINS,   KC_EQL,   KC_DQUO, KC_QUOT, KC_GRV,  KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, KC_BSLS,
-        _______, KC_SCLN,  KC_UNDS,   KC_PLUS,  KC_PIPE, KC_QUES, KC_PERC, KC_LBRC, KC_RBRC, KC_LT,   KC_GT,   _______,
-        _______, _______,  _______,   _______,  _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, KC_SLEP 
+        _______, KC_1,     KC_2,      KC_3,     KC_4,    KC_5,    KC_6,    KC_7,               KC_8,    KC_9,    KC_0,    _______,
+        _______, KC_COLN,  KC_MINS,   KC_EQL,   KC_DQUO, KC_QUOT, KC_GRV,  KC_LPRN,            KC_RPRN, KC_LCBR, KC_RCBR, _______,
+        _______, KC_TILD,  KC_UNDS,   KC_PLUS,  KC_PIPE, KC_BSLS, _______, KC_LBRC,            KC_RBRC, KC_LT,   KC_GT,   _______,
+        _______, _______,  _______,   _______,  _______, _______, _______, LT(_ADJUST,KC_BSPC), _______, _______, _______, _______ 
     ),
-    [_RAISE] = LAYOUT_ortho_4x12(
-        KC_TILD,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
-        KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_RCBR, KC_PIPE,
-        _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_LBRC, KC_RBRC, KC_PGUP, KC_PGDN, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BRID, KC_BRIU, KC_WAKE 
-    ),
-    [_NUMPAD] = LAYOUT_ortho_4x12(
-        TG(_NUMPAD), KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,        KC_0,    KC_BSPC,
-        _______,     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,       KC_F10,  KC_ENT,
-        _______,     KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______,     _______, _______,
-        _______,     _______, _______, _______, _______, _______, _______, _______, _______, TG(_NUMPAD), _______, _______
+    [_NAV] = LAYOUT_ortho_4x12(
+        _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,              KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+        _______, _______, _______, _______, _______,             _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_RCBR, KC_PIPE,
+        _______, _______, _______, _______, _______,             _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______,  _______,
+        _______, _______, _______, _______, LT(_ADJUST,KC_TAB),  _______, _______, _______, _______, _______, _______, _______ 
     ),
     [_ADJUST] = LAYOUT_ortho_4x12(
-        _______, _______, _______, RGB_SAI, _______, RGB_TOG, _______, KC_WH_D, _______, _______, KC_MPRV, DB_TOGG,
+        _______, QK_BOOT, _______, RGB_SAI, _______, RGB_TOG, _______, KC_WH_D, _______, _______, KC_MPRV, DB_TOGG,
         KC_BTN2, _______, RGB_SPI, KC_WH_U, RGB_MOD, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, KC_BTN1,
         _______, _______, _______, RGB_HUI, KC_MUTE, RGB_VAI, KC_MNXT, KC_MUTE, _______, _______, _______, _______,
-        QK_BOOT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PWR
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PWR
+    ),
+    [_NUMPAD] = LAYOUT_ortho_4x12(
+        _______, KC_LBRC, KC_7,    KC_8,    KC_9,  KC_RBRC, _______, _______, _______, _______, _______, _______,
+        _______, KC_SCLN, KC_4,    KC_5,    KC_6,  KC_EQL,  _______, _______, _______, _______, _______, _______,
+        _______, KC_GRV,  KC_1,    KC_2,    KC_3,  KC_BSLS, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, KC_DOT,  KC_0,  KC_UNDS, _______, _______, _______, _______, _______, _______ 
+    ),
+    [_FUNC] = LAYOUT_ortho_4x12(
+        _______, KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_PSCR, _______, _______, _______, _______, _______, _______,
+        _______, KC_F11,  KC_F4,   KC_F5,   KC_F6,   KC_SCRL, _______, _______, _______, _______, _______, _______,
+        _______, KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_PAUS, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, KC_SPC,  KC_TAB,  _______, _______, _______, _______, _______, _______ 
+    ),
+    [_MEDIA] = LAYOUT_ortho_4x12(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, KC_LALT, KC_LGUI, KC_LSFT, KC_LCTL, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, KC_MSTP, KC_MPLY, KC_MUTE, _______, _______, _______ 
+    ),
+    [_MOUSE] = LAYOUT_ortho_4x12(
+        _______, _______, _______, _______, _______, _______, KC_ACL0, KC_ACL1, KC_ACL2, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,
+        _______, _______, _______, _______, _______, _______, KC_WH_R, KC_WH_U, KC_WH_D, KC_WH_L, _______, _______,
+        _______, _______, _______, _______, _______, _______, KC_BTN2, KC_BTN1, KC_BTN3, _______, _______, _______ 
     )
 };
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-}
